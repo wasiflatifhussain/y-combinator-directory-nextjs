1e2d255c5d7cd52348fe973f950d71098ee7c075
wasiflatifhussain <wasiflh@connect.hku.hk>
2025-01-02 17:53:51 +0800

    added nextjs forms for search bar handling and reset

---
+++ yc-directory/components/SearchFormReset.tsx
@@ -0,0 +1,23 @@
+"use client";
+import React from 'react';
+import Link from 'next/link';
+import { X } from 'lucide-react';
+
+// this is client-sided as client resets the input form
+const SearchFormReset = () => {
+
+  const reset = () => {
+    const form = document.querySelector(".search-form") as HTMLFormElement;
+    if (form) form.reset();
+  };
+
+  return (
+    <button type="reset" onClick={reset}>
+      <Link href="/" className="search-btn text-white"> 
+        <X className='size-5' />
+      </Link>
+    </button>
+)
+};
+
+export default SearchFormReset;
