5cf9e68e95e547bd433c98a282c5b332233858d6
wasiflatifhussain <wasiflh@connect.hku.hk>
2025-01-04 18:18:55 +0800

    added editor's pick section

--- yc-directory/app/(root)/startup/[id]/page.tsx
+++ yc-directory/app/(root)/startup/[id]/page.tsx
@@ -1,6 +1,6 @@
 import { formatDate } from '@/lib/utils';
 import { client } from '@/sanity/lib/client';
-import { STARTUP_BY_ID_QUERY } from '@/sanity/lib/queries';
+import { PLAYLIST_BY_SLUG_QUERY, STARTUP_BY_ID_QUERY } from '@/sanity/lib/queries';
 import Image from 'next/image';
 import Link from 'next/link';
 import { notFound } from 'next/navigation';
@@ -8,6 +8,7 @@ import React, { Suspense } from 'react';
 import markdownit from 'markdown-it';
 import { Skeleton } from '@/components/ui/skeleton';
 import View from '@/components/View';
+import StartupCard, { StartupTypeCard } from '@/components/StartupCard';
 
 const md = new markdownit();
 
@@ -18,6 +19,8 @@ const Page = async ({params}: {params: Promise<{id: string}>}) => {
 
   const post = await client.fetch(STARTUP_BY_ID_QUERY, {id});
   if (!post) { return notFound(); }
+
+  const {select: editorPosts} = await client.fetch(PLAYLIST_BY_SLUG_QUERY, {slug: 'editor-picks'});
 
   const parsedContent = md.render(post?.pitch || '');
 
@@ -57,7 +60,18 @@ const Page = async ({params}: {params: Promise<{id: string}>}) => {
 
         <hr className='divider' />
 
-        {/* To-do: Editor Selected Startups */}
+        {/* Editor Selected Startups */}
+        {editorPosts?.length > 0 && (
+          <div className="max-w-4xl mx-auto">
+            <p className="text-30-semibold">Editor Picks</p>
+
+            <ul className="mt-7 card_grid-sm">
+              {editorPosts.map((post: StartupTypeCard, i: number) => (
+                <StartupCard key={i} post={post} />
+              ))}
+            </ul>
+          </div>
+        )}
 
         {/* PPR used here for views; use Suspense tag */}
         <Suspense fallback={<Skeleton className='view_skeleton' />}>
